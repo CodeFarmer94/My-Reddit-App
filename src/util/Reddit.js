769129286a826c363863eb4905a23266ffa5ff25
CODEFARMER <dmyakinchenko@gmail.com>
2023-03-20 20:34:18 +0100

    access token fixed

--- src/util/Reddit.js
+++ src/util/Reddit.js
@@ -2,16 +2,20 @@ import React, { useState, useEffect } from "react";
 
 const REDDIT_CLIENT_ID = "E8YjmRNAhz4lJY9ARSsC5A";
 const REDDIT_CLIENT_SECRET = "PhtmMSoqIyXE1O3a5Y5uOvEqcWB6_g";
-const REDDIT_REDIRECT_URI = "http://localhost:3000/"; // change this to your app's redirect URI
+const REDDIT_REDIRECT_URI = "http://localhost:3000/"; 
 
 function Reddit() {
- 
+  
 
+  const [accessToken, setAccessToken] = useState(null)
+  
   function authorize() {
-    // Construct the authorization URL
+    const urlParams = new URLSearchParams(window.location.search);
+    const code = urlParams.get("code");
     const authUrl = `https://www.reddit.com/api/v1/authorize?client_id=${REDDIT_CLIENT_ID}&response_type=code&state=state&redirect_uri=${encodeURIComponent(REDDIT_REDIRECT_URI)}&duration=temporary&scope=read`;
     // Redirect the user to the authorization URL
-    window.location.href = authUrl
+    if(!code)
+   { window.location.href = authUrl}
   }
 
 
@@ -19,10 +23,8 @@ function Reddit() {
     // Construct the token request URL
     const urlParams = new URLSearchParams(window.location.search);
     const code = urlParams.get("code");
-    console.log(code)
-    
     const tokenUrl = "https://www.reddit.com/api/v1/access_token";
-    const basicAuth = (`${REDDIT_CLIENT_ID}:${REDDIT_CLIENT_SECRET}`);
+    const basicAuth = btoa(`${REDDIT_CLIENT_ID}:${REDDIT_CLIENT_SECRET}`)
     const options = {
       method: "POST",
       headers: {
@@ -31,47 +33,35 @@ function Reddit() {
       },
       body: `grant_type=authorization_code&code=${code}&redirect_uri=${REDDIT_REDIRECT_URI}`
     };
-
     // Request the access token
     const response = await fetch(tokenUrl, options);
     const data = await response.json();
-    
     // Save the access token to local storage
     localStorage.setItem("reddit_access_token", data.access_token);
-
+    setAccessToken(data.access_token)
     // Clear the URL search params so it doesn't show the access token or code
     window.history.replaceState({}, "", window.location.pathname);
-
   }
-/*
-  async function getAccessToken() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const code = urlParams.get("code");
-    if (code) {
-      await getToken(code);
-    }
-    else{
-      console.log("no code")
-    }
-    // Try to get the access token from local storage
-    const accessToken = localStorage.getItem("reddit_access_token");
     
-    // If there is no access token in local storage, authorize the user
-    if (!accessToken) {
-      authorize();
-      console.log(accessToken)
+    useEffect(()=>{
+      console.log("effect ran")
+      const urlParams = new URLSearchParams(window.location.search);
+      const code = urlParams.get("code");
+      if(!code)
+      {authorize()
+      }
+      getToken()
     }
+    ,[])
 
-  }
+  
 
-  useEffect(() => {
-    getAccessToken();
-  }, []);
-*/
   return (
     <div>
      <button onClick = {authorize}>Autorizhe</button>
      <button onClick = {getToken}> Get Token</button>
+     Storage:{localStorage.getItem("reddit_access_token")}
+     State:{accessToken}
     </div>
   );
 }
